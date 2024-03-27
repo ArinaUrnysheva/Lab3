2b1b98bdc793f1490f9c1378d0d8db618f734594
ArinaUrnysheva <148492062+ArinaUrnysheva@users.noreply.github.com>
2024-03-27 18:30:42 +0300

    Add files via upload

---
+++ лаба 3 kdvlsdmglsrmbl.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Car { //базовый класс - "Машина"
+protected:
+	string name; //марка машины
+	unsigned int speed; //максимальная скорость машины
+public:
+	Car(string name, unsigned int speed) { //конструктор для инициализации полей
+		this->name = name;
+		this->speed = speed;
+	}
+
+	virtual void print() {
+		cout << "name: " << name << endl << "max speed: " << speed << endl;
+	}
+};
+
+class Bus : //производный класс - "Автобус"
+	public Car {
+protected:
+	unsigned int passengers; //дополнительное поле - максимальное число пассажиров
+public:
+	Bus(string name, unsigned int speed, unsigned int passengers) : //конструктор
+		Car(name, speed) { //явный вызов конструктора базового класса
+		this->passengers = passengers;
+	}
+	void print() { //переопределенный метод print()
+		Car::print();
+		cout << "max amount of passengers of bus: " << passengers << endl; //допечатываем поле passengers
+	}
+};
+
+int main() {
+	Car car{ "mark 1", 200 };
+	car.print();
+
+	Bus bus{ "mark 2", 100, 50 };
+	bus.print();
+
+	return 0;
+}
